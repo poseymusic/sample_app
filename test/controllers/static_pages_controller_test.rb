a49d7fcad403cdadc13f39a90b1cdcc86417589d
Ken Posey <kennethwposey@yahoo.com>
2015-10-13 20:22:47 -0700

    finnish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,34 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-=begin
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-  end
-=end
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
-
-
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | CS 232 Ruby on Rails Development"
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
