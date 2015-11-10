70252f8935088917f2faeac3ced6f1a3eb35a21b
Ken Posey <kennethwposey@yahoo.com>
2015-11-09 22:48:48 -0800

    UNcommnet debugger

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save      
+    if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user   #same as saying "redirect_to user_url(@user)"
     else
