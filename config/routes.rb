75675827ad4eb567cdb3b798137a8518abb2f8f0
Ken Posey <kennethwposey@yahoo.com>
2015-11-09 21:50:03 -0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+#signup.html.erb to controller: user action: new
   get 'signup'  => 'users#new'
+
+  resources :users
   #root 'static_pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
