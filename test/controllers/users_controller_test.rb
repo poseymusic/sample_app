51f3d5cb2076e5d1c8faab35374ce07f5eaaae9b
Ken Posey <kennethwposey@yahoo.com>
2015-12-10 18:44:12 -0800

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,20 @@ class UsersControllerTest < ActionController::TestCase
     @other_user = users(:archer)
   end
 
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+
+  # test "should not allow the admin attribute to be edited via the web" do
+  #   log_in_as(@other_user)
+  #   assert_not @other_user.admin?
+  #   patch :update, id: @other_user, user: { password:              "foobar",
+  #                                           password_confirmation: "foobar",
+  #                                           admin: true }
+  #   assert_not @other_user.FILL_IN.admin?
+  # end
+
   test "should get new" do
     get :new
     assert_response :success
@@ -37,4 +51,19 @@ class UsersControllerTest < ActionController::TestCase
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end  
 end
