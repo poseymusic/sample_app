b6094c880f71baffa67f178bce1530b4cd68ddc5
Ken Posey <kennethwposey@yahoo.com>
2015-12-21 23:08:34 -0800

    Add user microposts

--- db/migrate/20151219224359_add_activation_to_users.rb
+++ db/migrate/20151219224359_add_activation_to_users.rb
@@ -1,15 +1,15 @@
 class AddActivationToUsers < ActiveRecord::Migration
   #used to remove the columns
-  def up
-    remove_column :users, :activation_digest
-    remove_column :users, :activated
-    remove_column :users, :activated_at
-  end
-
-  def down
-    add_column :users, :activation_digest, :string
-    add_column :users, :activated, :boolean, default: false
-    add_column :users, :activated_at, :datetime
-  end
+  # def up
+  #   remove_column :users, :activation_digest
+  #   remove_column :users, :activated
+  #   remove_column :users, :activated_at
+  # end
+  #
+  # def down
+  #   add_column :users, :activation_digest, :string
+  #   add_column :users, :activated, :boolean, default: false
+  #   add_column :users, :activated_at, :datetime
+  # end
 
 end
