5d1316e688efd9c1ab81ad0d89bf06811618e1a6
Ken Posey <kennethwposey@yahoo.com>
2015-12-22 16:56:54 -0800

    more

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
-50.times do |n|
+6.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -18,8 +18,9 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end
 
+
  users = User.order(:created_at).take(6)
- 20.times do
+ 3.times do
    content = Faker::Lorem.sentence(5)
    users.each { |user| user.microposts.create!(content: content) }
  end
