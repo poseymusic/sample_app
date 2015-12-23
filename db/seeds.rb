265c2e35c78292b3cc2316c2eff44e700615603d
Ken Posey <kennethwposey@yahoo.com>
2015-12-22 16:27:24 -0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ User.create!(name:  "Example User",
              admin:     true,
              activated: true,
              activated_at: Time.zone.now)
-
-99.times do |n|
+             
+10.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -18,8 +18,8 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 
  users = User.order(:created_at).take(6)
- 50.times do
+ 5.times do
    content = Faker::Lorem.sentence(5)
    users.each { |user| user.microposts.create!(content: content) }
- end                             
+ end
 end
